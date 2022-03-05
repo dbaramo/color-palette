6b0c9c1fef8da8ec314f0a21bca9d7fe6bf08ed8
Daniel Baramo <dbaramo@gmail.com>
2022-03-05 17:12:46 -0500

    made navbar && create a palette responsive

--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,5 +1,8 @@
 import { DRAWER_WIDTH } from "../constants";
+import sizes from "./sizes";
+
 const drawerWidth = DRAWER_WIDTH;
+
 const styles = theme => ({
   root: {
     display: "flex"
@@ -33,10 +36,17 @@ const styles = theme => ({
     marginRight: "1rem",
     "& a": {
       textDecoration: "none"
+    },
+    [sizes.down("xs")]: {
+      marginRight: "0.5rem"
     }
   },
   button: {
-    margin: "0 0.5rem"
+    margin: "0 0.5rem",
+    [sizes.down("xs")]: {
+      margin: "0 0.2rem",
+      padding: "0.3rem"
+    }
   }
 });
 
