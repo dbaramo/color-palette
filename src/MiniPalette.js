dd73a9332efed33e965de919e317bde317d94fde
Daniel Baramo <dbaramo@gmail.com>
2022-03-05 13:54:15 -0500

    added delete icon

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,5 +1,6 @@
 import { withStyles } from "@material-ui/styles";
 import styles from "./styles/MiniPaletteStyles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function MiniPalette(props){
     const { classes, palette } = props;
@@ -13,6 +14,12 @@ function MiniPalette(props){
 
     return(
         <div className={classes.root} onClick={props.handleClick}>
+            <div className={classes.delete}>
+                <DeleteIcon
+                    className={classes.deleteIcon}
+                    style={{ transition: "all 0.3s ease-in-out" }}
+                />
+            </div>
             <div className={classes.colors}>{miniColorBoxes}</div>
             <h5 className={classes.title}>{palette.paletteName} 
                 <span className={classes.emoji}>{palette.emoji}</span>
